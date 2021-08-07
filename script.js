dd6ca33e572764acf94bf22e49e43699cd3d5aea
leolousa <leolousa@hotmail.com>
2021-08-07 13:11:10 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.querySelector("button")
+const text = document.querySelector(".text")
+
+const recognition = createRecognition()
+let listening = false
+
+
+
+button.addEventListener('click', e => {
+  if (!recognition) return
+
+  listening ? recognition.stop() : recognition.start()
+
+  button.innerHTML = listening ? 'Aperte para falar' : 'Parar de escutar'
+  button.classList.toggle('bg-purple-200')
+  button.classList.toggle('text-red-500')
+
+})
+
+function createRecognition() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  const recognition = SpeechRecognition !== undefined ? new SpeechRecognition() : null
+
+  if (!recognition) {
+    text.innerHTML = "Speech Recognition is not found!"
+    return null
+  }
+
+  recognition.lang = "pt_BR"
+  recognition.continuous = true
+  recognition.onstart = () => listening = true
+  recognition.onend = () => listening = false
+  recognition.onerror = e => console.log('error:', e);
+  //recognition.onresult = e => text.innerHTML = e.results[0][0].transcript;
+  recognition.onresult = e => {
+    for (var i = e.resultIndex; i < e.results.length; ++i) {
+      if (e.results[i].isFinal) {
+        text.innerHTML += e.results[i][0].transcript;
+      }
+    }
+  }
+
+
+  return recognition
+
+}
